3999452606ac5e53809b2b3035afd78f14efaf8f
mohamedadly1 <162830057+mohamedadly1@users.noreply.github.com>
2024-10-11 21:53:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.articles-slider', {
+    navigation: {
+        nextEl: '.news-slider-next',
+        prevEl: '.news-slider-prev',
+    },
+    // Additional swiper options...
+});
